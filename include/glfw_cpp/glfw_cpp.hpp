7a408b73e4a9df46783cc9f552dc39af7a9fcc4c
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-02-01 10:49:54 +0700

    Fix missing error.hpp include in glfw_cpp.hpp header

--- include/glfw_cpp/glfw_cpp.hpp
+++ include/glfw_cpp/glfw_cpp.hpp
@@ -1,11 +1,12 @@
 #ifndef GLFW_CPP_HPP_23OERFAF7EW
 #define GLFW_CPP_HPP_23OERFAF7EW
 
-#include "glfw_cpp/instance.hpp"
-#include "glfw_cpp/input.hpp"
+#include "glfw_cpp/error.hpp"
 #include "glfw_cpp/event.hpp"
-#include "glfw_cpp/window_manager.hpp"
-#include "glfw_cpp/window.hpp"
+#include "glfw_cpp/input.hpp"
+#include "glfw_cpp/instance.hpp"
 #include "glfw_cpp/monitor.hpp"
+#include "glfw_cpp/window.hpp"
+#include "glfw_cpp/window_manager.hpp"
 
 #endif /* end of include guard: GLFW_CPP_HPP_23OERFAF7EW */
