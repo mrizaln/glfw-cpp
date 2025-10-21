ee4b804214ae49b96faf9bbf3633f844940289b1
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-10-21 13:09:37 +0700

    Add imgui example

--- example/conanfile.py
+++ example/conanfile.py
@@ -1,16 +1,18 @@
 from conan import ConanFile
-from conan.tools.cmake import cmake_layout
+from conan.tools.cmake import cmake_layout, CMakeToolchain
+from os import path
 
 
 class Recipe(ConanFile):
     settings = ["os", "compiler", "build_type", "arch"]
-    generators = ["CMakeToolchain", "CMakeDeps"]
+    generators = ["CMakeDeps"]
     requires = [
         "fmt/10.2.1",
         "glad/0.1.36",
         "glbinding/3.3.0",
         "glfw/3.4",
         "glm/0.9.9.8",
+        "imgui/1.90.5",
         "khrplatform/cci.20200529",
         "linmath.h/cci.20220619",
         "stb/cci.20230920",
@@ -21,3 +23,10 @@ class Recipe(ConanFile):
 
     def layout(self):
         cmake_layout(self)
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["IMGUI_BINDINGS_DIR"] = path.join(
+            self.dependencies["imgui"].package_folder, "res", "bindings"
+        )
+        tc.generate()
---
+++ example/source/new/imgui.cpp
@@ -0,0 +1,73 @@
+#include <glbinding/gl/gl.h>
+#include <glbinding/glbinding.h>
+#include <glfw_cpp/extra/imgui.hpp>
+#include <glfw_cpp/glfw_cpp.hpp>
+#include <imgui_impl_opengl3.h>
+
+auto render_thread(glfw_cpp::Window& window, glfw_cpp::extra::ImguiInterceptor& interceptor)
+{
+    window.bind();
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    // replacing `ImGui_ImplGlfw_InitForOpenGL()`
+    auto imgui_glfw = glfw_cpp::extra::init_imgui_for_opengl(interceptor, window.handle());
+    ImGui_ImplOpenGL3_Init();
+
+    gl::glClearColor(0.1f, 0.1f, 0.11f, 1.0f);
+
+    while (not window.should_close()) {
+        namespace ev = glfw_cpp::event;
+        window.poll().visit(ev::Overload{
+            [&](const ev::KeyPressed& k) {
+                using K = glfw_cpp::KeyCode;
+                using S = glfw_cpp::KeyState;
+                if (k.key == K::Q and k.state == S::Press) {
+                    window.request_close();
+                }
+            },
+            [&](const auto&) { /* do nothing */ },
+        });
+
+        gl::glClear(gl::GL_COLOR_BUFFER_BIT);
+
+        ImGui_ImplOpenGL3_NewFrame();
+        imgui_glfw.new_frame();    // replacing `ImGui_ImplGlfw_NewFrame()`
+        ImGui::NewFrame();
+
+        ImGui::ShowDemoWindow();
+
+        ImGui::Render();
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        window.display();
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    imgui_glfw.shutdown();    // replacing `ImGui_ImplGlfw_Shutdown()`
+}
+
+int main()
+{
+    auto api = glfw_cpp::api::OpenGL{
+        .major   = 3,
+        .minor   = 3,
+        .profile = glfw_cpp::api::gl::Profile::Core,
+        .loader  = [](auto, auto proc) { glbinding::initialize(proc); },
+    };
+
+    auto glfw = glfw_cpp::init(api);
+
+    auto interceptor = glfw_cpp::extra::ImguiInterceptor{};
+    auto wm          = glfw->create_window_manager(&interceptor);
+
+    auto window        = wm->create_window({}, "hello imgui", 1280, 720, false);
+    auto window_thread = std::jthread{ render_thread, std::ref(window), std::ref(interceptor) };
+
+    while (wm->has_window_opened()) {
+        using glfw_cpp::operator""_fps;
+        wm->poll_events(120_fps);
+    }
+}
