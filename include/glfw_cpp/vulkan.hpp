11ccae3b62ada0a72a8ee90b18f088052c7efe2c
Muhammad Rizal Nurromdhoni <mrizaln2000@gmail.com>
2025-12-09 06:28:03 +0700

    Remove linking requirement for Vulkan
    Vulkan support is out of the box now just like GLFW, thus
    `GLFW_CPP_VULKAN_SUPPORT` option is also removed. No linking with vulkan
    loader necessary.
    
    Also, I added missing `init_vulkan_loader()` function, though untested
    (I need macOS to do that).

--- include/glfw_cpp/vulkan.hpp
+++ include/glfw_cpp/vulkan.hpp
@@ -1,47 +1,171 @@
 #ifndef GLFW_CPP_VULKAN_HPP
 #define GLFW_CPP_VULKAN_HPP
 
-#if not defined(GLFW_CPP_VULKAN_SUPPORT)
-#    error "Vulkan support is not enabled"
+#if defined(GLFW_CPP_INCLUDE_VULKAN_H)
+    #include <vulkan/vulkan.h>
+#endif
+#if defined(GLFW_CPP_INCLUDE_VULKAN_HPP)
+    #include <vulkan/vulkan.hpp>
 #endif
 
-#if not defined(VK_VERSION_1_0) and not defined(VULKAN_HPP)
-#    error "Include Vulkan headers (vulkan.h or vulkan.hpp) before including this header"
-#endif
+#include <span>
 
-#include "glfw_cpp/window.hpp"
+namespace glfw_cpp
+{
+    class Window;
+}
 
 namespace glfw_cpp::vk
 {
-    using VkProc = void (*)(void);
-
-    bool                     vulkan_supported();
-    std::vector<const char*> get_required_instance_extensions();
+    using Proc = void (*)(void);
 
 #if defined(VK_VERSION_1_0)
-    VkProc get_instance_proc_address(VkInstance instance, const char* proc_name);
+    /**
+     * @brief set the `vkGetInstanceProcAddr` function that GLFW will use for all Vulkan related entry point
+     * queries.
+     *
+     * @param loader The address of the function to use, or `nullptr`.
+     *
+     * @thread_safety This function must be called from the main thread.
+     *
+     * @warning This function must be called before `init()`.
+     *
+     * If set to NULL, GLFW will try to load the Vulkan loader dynamically by its standard name and get this
+     * function from there. This is the default behavior.
+     *
+     * This feature is mostly useful on macOS, if your copy of the Vulkan loader is in a location where GLFW
+     * cannot find it through dynamic loading, or if you are still using the static library version of the
+     * loader.
+     */
+    void init_vulkan_loader(PFN_vkGetInstanceProcAddr loader);
+#endif
 
+    /**
+     * @brief Check whether the Vulkan loader and any minimally functional ICD have been found.
+     *
+     * @return True if Vulkan is minimally available, false otherwise.
+     *
+     * @thread_safety This function may be called from any thread.
+     *
+     * @throw error::NotInitialized If glfw is not initialized yet.
+     */
+    bool vulkan_supported();
+
+    /**
+     * @brief Return an span of names of Vulkan instance extensions required by GLFW for creating Vulkan
+     * surfaces for GLFW windows
+     *
+     * @thread_safety This function may be called from any thread.
+     */
+    std::span<const char*> get_required_instance_extensions();
+
+#if defined(VK_VERSION_1_0)
+    /**
+     * @brief Return the address of the specified vulkan core or extension function for the specified
+     * instance.
+     *
+     * @param instance The vulkan instance to query or `nullptr` to retrieve functions related to instance
+     * creation.
+     * @param proc_name The ASCII encoded name of the function.
+     * @return The address of the function or `nullptr` if an error occurred.
+     *
+     * @thread_safety This function may be called from any thread.
+     *
+     * @throw error::NotInitialized If GLFW is not initialized yet.
+     * @throw error::ApiUnavailable If Vulkan API is not available.
+     *
+     * If you need to call this function from C (which can't handle exception) you may want to use `noexcept`
+     * version of this function: `get_instace_proc_address_noexcept()`.
+     */
+    Proc get_instance_proc_address(VkInstance instance, const char* proc_name);
+
+    /**
+     * @brief Return the address of the specified vulkan core or extension function for the specified
+     * instance.
+     *
+     * @param instance The vulkan instance to query or `nullptr` to retrieve functions related to instance
+     * creation.
+     * @param proc_name The ASCII encoded name of the function.
+     * @return The address of the function or `nullptr` if an error occurred.
+     *
+     * @thread_safety This function may be called from any thread.
+     *
+     * The error will be logged but this function won't throw to preserve the behavior from the underlying
+     * function. The possible errors are:
+     * - `ErrorCode::NotInitialized`, and
+     * - `ErrorCode::ApiUnavailable`.
+     *
+     * If you don't mind the function throws, use `get_proc_address()` instead.
+     */
+    Proc get_instance_proc_address_noexcept(VkInstance instance, const char* proc_name) noexcept;
+
+    /**
+     * @brief Check whether the specified queue family of the specified physical device supports presentation
+     * to the platform GLFW was built for.
+     *
+     * @param instance The instance that the physical device belongs to.
+     * @param device The physical device that the queue family belongs to.
+     * @param queue_family The index of the queue family to query.
+     * @return True if the queue family supports presentation, false otherwise.
+     *
+     * @thread_safety This function may be called from any thread.
+     *
+     * @throw error::NotInitialized If GLFW is not initialized yet.
+     * @throw error::ApiUnavailable If Vulkan API is not available.
+     * @throw error::PlatformError If a platform-specific error occurred.
+     */
     bool get_physical_device_presentation_support(
         VkInstance       instance,
         VkPhysicalDevice device,
         uint32_t         queue_family
     );
 
+    /**
+     * @brief Create a vulkan surface for the specified window.
+     *
+     * @param window The window to create the surface for.
+     * @param instance The Vulkan instance to create the surface in.
+     * @param allocator The allocator to use or `nullptr` to use the default allocator.
+     * @param surface Where to store the handle of the surface.
+     * @return `VK_SUCCESS` if successful, or Vulkan error code if an error occurred.
+     */
     VkResult create_surface(
         const Window&                window,
         VkInstance                   instance,
         const VkAllocationCallbacks* allocator,
         VkSurfaceKHR*                surface
     );
-
 #endif    // VK_VERSION_1_0
 
 #ifdef VULKAN_HPP
-    inline VkProc get_instance_proc_address(const ::vk::Instance& instance, const char* proc_name)
+    /**
+     * @brief Helper function for application that uses `vulkan.hpp` header.
+     *
+     * See the other overload of `get_instance_proc_address()` to see the documentation.
+     */
+    inline Proc get_instance_proc_address(const ::vk::Instance& instance, const char* proc_name)
     {
         return get_instance_proc_address(static_cast<VkInstance>(instance), proc_name);
     }
 
+    /**
+     * @brief Helper function for application that uses `vulkan.hpp` header.
+     *
+     * See the other overload of `get_instance_proc_address_noexcept()` to see the documentation.
+     */
+    inline Proc get_instance_proc_address_noexcept(
+        const ::vk::Instance& instance,
+        const char*           proc_name
+    ) noexcept
+    {
+        return get_instance_proc_address_noexcept(static_cast<VkInstance>(instance), proc_name);
+    }
+
+    /**
+     * @brief Helper function for application that uses `vulkan.hpp` header.
+     *
+     * See the other overload of `get_physical_device_presentation_support()` to see the documentation.
+     */
     inline bool get_physical_device_presentation_support(
         const ::vk::Instance&       instance,
         const ::vk::PhysicalDevice& device,
@@ -53,6 +177,11 @@ namespace glfw_cpp::vk
         );
     }
 
+    /**
+     * @brief Helper function for application that uses `vulkan.hpp` header.
+     *
+     * See the other overload of `create_surface()` to see the documentation.
+     */
     inline ::vk::ResultValue<::vk::SurfaceKHR> create_surface(
         const Window&                             window,
         const ::vk::Instance&                     instance,
@@ -72,7 +201,6 @@ namespace glfw_cpp::vk
 
         return { ::vk::Result(result), ::vk::SurfaceKHR(surface) };
     }
-
 #endif    // VULKAN_HPP
 }
 
--- source/vulkan.cpp
+++ source/vulkan.cpp
@@ -1,39 +1,96 @@
-#define GLFW_INCLUDE_VULKAN
-#include <GLFW/glfw3.h>
+// vk tricks
+// ---------
+#include <cstdint>
 
-#include "glfw_cpp/vulkan.hpp"
+#ifndef VK_USE_64_BIT_PTR_DEFINES
+    #if defined(__LP64__) || defined(_WIN64) || (defined(__x86_64__) && !defined(__ILP32__))                 \
+        || defined(_M_X64) || defined(__ia64) || defined(_M_IA64) || defined(__aarch64__)                    \
+        || defined(__powerpc64__) || (defined(__riscv) && __riscv_xlen == 64)
+        #define VK_USE_64_BIT_PTR_DEFINES 1
+    #else
+        #define VK_USE_64_BIT_PTR_DEFINES 0
+    #endif
+#endif
+
+#define VK_DEFINE_HANDLE(object) typedef struct object##_T* object;
+#ifndef VK_DEFINE_NON_DISPATCHABLE_HANDLE
+    #if (VK_USE_64_BIT_PTR_DEFINES == 1)
+        #define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef struct object##_T* object;
+    #else
+        #define VK_DEFINE_NON_DISPATCHABLE_HANDLE(object) typedef uint64_t object;
+    #endif
+#endif
+
+// is this extern C needed? should I simplify the typedef by using using instead?
+extern "C" {
+// vk types definition for use in this unit only
+
+typedef enum VkResult
+{
+} VkResult;
+
+typedef struct VkExtensionProperties
+{
+    char     extensionName[256];
+    uint32_t specVersion;
+} VkExtensionProperties;
+
+VK_DEFINE_HANDLE(VkInstance);
+VK_DEFINE_HANDLE(VkPhysicalDevice);
+
+VK_DEFINE_NON_DISPATCHABLE_HANDLE(VkSurfaceKHR);
+
+typedef struct VkAllocationCallbacks VkAllocationCallbacks;
+
+typedef void (*PFN_vkVoidFunction)(void);
+
+typedef PFN_vkVoidFunction (*PFN_vkGetInstanceProcAddr)(VkInstance, const char*);
+typedef VkResult (*PFN_vkEnumerateInstanceExtensionProperties)(const char*, uint32_t*, VkExtensionProperties*);
+}
+
+// tricks <glfw_cpp/vulkan.hpp> and GLFW into thinking that it has Vulkan included so that I can use the
+// functions defined in GLFW as long as I have the required types defined
+#define VK_VERSION_1_0
+// ---------
+
 #include "glfw_cpp/window.hpp"
 
-#include <vector>
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+#include "util.hpp"
 
 namespace glfw_cpp::vk
 {
+    using Proc = void (*)(void);
+
+    void init_vulkan_loader(PFN_vkGetInstanceProcAddr loader)
+    {
+        glfwInitVulkanLoader(loader);
+    }
+
     bool vulkan_supported()
     {
         return glfwVulkanSupported();
     }
 
-    std::vector<const char*> get_required_instance_extensions()
+    std::span<const char*> get_required_instance_extensions()
     {
         auto count             = uint32_t{};
         auto p_extension_names = glfwGetRequiredInstanceExtensions(&count);
-
-        auto extension_names = std::vector<const char*>{};
-        extension_names.reserve(count);
-        for (uint32_t i = 0; i < count; ++i) {
-            extension_names.push_back(p_extension_names[i]);
-        }
-        return extension_names;
+        return { p_extension_names, count };
     }
 
-    VkResult create_surface(
-        const Window&                window,
-        VkInstance                   instance,
-        const VkAllocationCallbacks* allocator,
-        VkSurfaceKHR*                surface
-    )
+    Proc get_instance_proc_address(VkInstance instance, const char* proc_name)
     {
-        return glfwCreateWindowSurface(instance, window.handle(), allocator, surface);
+        auto addr = glfwGetInstanceProcAddress(instance, proc_name);
+        util::check_glfw_error();
+        return addr;
+    }
+
+    Proc get_instance_proc_address_noexcept(VkInstance instance, const char* proc_name) noexcept
+    {
+        return glfwGetInstanceProcAddress(instance, proc_name);
     }
 
     bool get_physical_device_presentation_support(
@@ -45,8 +102,13 @@ namespace glfw_cpp::vk
         return glfwGetPhysicalDevicePresentationSupport(instance, device, queue_family);
     }
 
-    VkProc get_instance_proc_address(VkInstance instance, const char* proc_name)
+    VkResult create_surface(
+        const Window&                window,
+        VkInstance                   instance,
+        const VkAllocationCallbacks* allocator,
+        VkSurfaceKHR*                surface
+    )
     {
-        return glfwGetInstanceProcAddress(instance, proc_name);
+        return glfwCreateWindowSurface(instance, window.handle(), allocator, surface);
     }
 }
